65f0043eab3c215aafb29e14335312ca10000c1f
shrall <shrallvierdo@gmail.com>
2024-01-11 12:34:44 +0700

    feat: setup custom font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,15 +17,17 @@ export default function Home() {
   return (
     <Layout>
       <Seo title="Home" />
-      <main className="w-screen min-h-screen flex flex-col items-center justify-center gap-4 px-32 py-12">
+      <section className="w-screen min-h-screen flex flex-col items-center justify-center gap-4 px-32 py-12">
         <Card className="w-3/4 min-h-[70vh] bg-ocean bg-cover flex flex-col items-center justify-center">
           <CardHeader className="flex flex-col items-center justify-center">
             <CardTitle className="text-ocean-light-10">
               <Image
                 src="/images/logo-text.png"
                 alt="Ocean Logo"
-                width="200"
-                height="100"
+                width={0}
+                height={0}
+                sizes="100vw"
+                style={{ width: "200px", height: "auto" }}
               />
             </CardTitle>
             <CardDescription className="text-xl text-ocean-light-10 font-semibold ml-2">
@@ -40,7 +42,7 @@ export default function Home() {
             </Link>
           </CardContent>
         </Card>
-      </main>
+      </section>
     </Layout>
   );
 }
