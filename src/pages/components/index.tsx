16cb41a3a763270f0909433002ec46ddc9832c40
shrall <shrallvierdo@gmail.com>
2024-01-16 14:42:26 +0700

    feat: add sonner & warning button color

--- src/pages/components/index.tsx
+++ src/pages/components/index.tsx
@@ -1,28 +1,29 @@
-import { ArrowLeft } from "lucide-react";
-import Link from "next/link";
+import { ArrowLeft } from 'lucide-react';
+import Link from 'next/link';
 
-import Seo from "@/components/seo";
-import { Button } from "@/components/ui/button";
+import Seo from '@/components/seo';
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
+} from '@/components/ui/card';
 
-import Avatars from "./avatars";
-import Badges from "./badges";
-import Buttons from "./buttons";
-import Icons from "./icons";
-import Skeletons from "./skeletons";
+import Avatars from './avatars';
+import Badges from './badges';
+import Buttons from './buttons';
+import Icons from './icons';
+import Skeletons from './skeletons';
+import Toast from './toast';
 
 export default function Components() {
   return (
-    <main className="w-screen min-h-screen flex flex-col px-32 py-12 gap-4">
-      <Seo templateTitle="Components" siteName="Ocean Starter Template" />
-      <Link href="/">
-        <Button variant="link" className="mr-auto">
-          <ArrowLeft className="size-5 mr-2" /> Go Back
+    <main className='flex min-h-screen w-screen flex-col gap-4 px-32 py-12'>
+      <Seo templateTitle='Components' siteName='Ocean Starter Template' />
+      <Link href='/'>
+        <Button variant='link' className='mr-auto'>
+          <ArrowLeft className='mr-2 size-5' /> Go Back
         </Button>
       </Link>
       <Card>
@@ -43,6 +44,8 @@ export default function Components() {
       <Icons />
       {/* //NOTE - Skeletons */}
       <Skeletons />
+      {/* //NOTE - Toast */}
+      <Toast />
     </main>
   );
 }
---
+++ src/pages/components/toast.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import { toast } from 'sonner';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+function Toast() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className='flex'>
+          Toast/
+          <Link
+            href='https://ui.shadcn.com/docs/components/sonner'
+            className='flex text-ocean-primary-10 hover:underline'
+          >
+            Sonner
+          </Link>
+        </CardTitle>
+      </CardHeader>
+      <CardContent className='grid grid-cols-2 gap-x-2 gap-y-4'>
+        <div className='flex items-center gap-2'>
+          <Button variant='default' onClick={() => toast('Hello World!')}>
+            Click me
+          </Button>
+          <Button
+            variant='default'
+            color='success'
+            onClick={() => toast.success('Hello World!')}
+          >
+            Success
+          </Button>
+          <Button
+            variant='default'
+            color='warning'
+            onClick={() => toast.warning('Hello World!')}
+          >
+            Warning
+          </Button>
+          <Button
+            variant='default'
+            color='danger'
+            onClick={() => toast.error('Hello World!')}
+          >
+            Error
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Toast;
